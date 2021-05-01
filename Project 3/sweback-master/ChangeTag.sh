727c4353485e5d2835c46b79e20aafe945940e0e
Mithilaesh Jayakumar <mithilaesh.mithun96@gmail.com>
2021-05-01 18:50:07 -0400

    updated files

---
+++ Project 3/sweback-master/ChangeTag.sh
@@ -0,0 +1,5 @@
+#SWE 645 Assignment2, Team members: Avinash[G01163980], Rashi[G01225299], Rushil[G01203932], Mithilaesh[G01206238].
+
+#it adds new tag version for each build made and deployed to pods
+
+sed "s/tagVersion/$1/g" pods.yml > swepods.yml
---
+++ Project 3/sweback-master/src/main/webapp/index.jsp
@@ -0,0 +1,6 @@
+<html>
+<body>
+<h2>Hello World!</h2>
+<a href="webapi/api" >click here</a>
+</body>
+</html>
